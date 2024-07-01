6838fb3bee00f8fe2799bbb72eb1f380ce36d544
Pip Turner <pip.turner@ultraleap.com>
2024-07-01 01:03:55 +0100

    nicer theme selection + add dark theme

--- js/themeManager.js
+++ js/themeManager.js
@@ -13,3 +13,54 @@ function toggleTheme(evt) {
         }, 500);
     }, 500);
 }
+
+var darkRadioButton = document.getElementById("themeRadioDark");
+var whiteRadioButton = document.getElementById("themeRadioWhite");
+var yellowRadioButton = document.getElementById("themeRadioYellow");
+
+darkRadioButton.addEventListener('input', setThemeButton);
+whiteRadioButton.addEventListener('input', setThemeButton);
+yellowRadioButton.addEventListener('input', setThemeButton);
+initialThemeSet();
+
+
+function initialThemeSet() {
+    var theme = localStorage.getItem("theme");
+
+    if (theme == null) {
+        setTheme("yellow")
+    }
+    else {
+        setTheme(theme);
+    }
+
+    if (theme == "yellow") {
+        yellowRadioButton.checked = true
+    } else if (theme == "dark") {
+        darkRadioButton.checked = true
+    } else if (theme == "white") {
+        whiteRadioButton.checked = true
+    }
+}
+
+function setThemeButton(button) {
+    setTheme(button.target.value);
+}
+
+function setTheme(theme) {
+    localStorage.setItem("theme", theme);
+
+    document.getElementById(`portfolio-container`).classList.add('pre-animation');
+
+    setTimeout(function () {
+        document.documentElement.className = theme;
+        setTimeout(function () {
+            document.getElementById(`portfolio-container`).classList.remove('pre-animation');
+        }, 500);
+    }, 500);
+}
+
+function setThemeInstant(theme) {
+    localStorage.setItem("theme", theme);
+    document.documentElement.className = theme;
+}
